2b893877cc7d712b3ee14e053241007a6d11acbc
toniodin <tonichiqueroencinas@gmail.com>
2023-10-21 22:20:45 +0200

    Creación del proyecto
    No se conecta a la BD aún y no se porque

---
+++ Proyecto_T_A_G/src/bd/Conexion.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Conexion {
+    private Connection connection;
+    private String host     = "localhost";
+    private String usuario  = "root";
+    private String password = "usbw";
+    private String nombreBD = "proyecto_t_a_g";
+    private String url      = "http://localhost:8080/phpmyadmin/proyecto_t_a_g";
+    
+    public Connection DatabaseConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            this.connection = DriverManager.getConnection(url,this.usuario,this.password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return this.connection;
+    }
+}
---
+++ Proyecto_T_A_G/src/proyecto_t_a_g/Proyecto_T_A_G.java
@@ -0,0 +1,6 @@
+package proyecto_t_a_g;
+
+public class Proyecto_T_A_G {
+    public static void main(String[] args) {
+    } 
+}
