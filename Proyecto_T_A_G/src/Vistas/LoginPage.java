b20ae5fa6389d6b598c688735ad518c7d2973fce
Andreu6 <suarezbellavistaandreu@gmail.com>
2023-10-25 12:55:44 +0200

    No pueda hacer mas grande la ventana

--- Proyecto_T_A_G/src/Vistas/LoginPage.java
+++ Proyecto_T_A_G/src/Vistas/LoginPage.java
@@ -61,6 +61,7 @@ public class LoginPage extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Login");
+        setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(255, 222, 89));
